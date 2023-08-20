971c0199e56a57394021adc601034c66ce3a2530
Gautham Shinde <gautham.kumar@msds.christuniversity.in>
2023-08-20 18:34:18 +0530

    Update Mask_Detection_Buzzer.ino

--- Arduino/Mask_Detection_Buzzer/Mask_Detection_Buzzer.ino
+++ Arduino/Mask_Detection_Buzzer/Mask_Detection_Buzzer.ino
@@ -1,20 +1,46 @@
-#include <SPI.h>
 #include <Ethernet.h>
+#include <Wire.h>
 #include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include "HardwareSerial.h"
 
-// Ethernet shield configuration
-byte mac[] = { 0xDE, 0xAD, 0xBE, 38, 0xFE, 0xED };
-IPAddress ip(172, 16, 0, 100); // Change this to your MQTT broker's IP address
-IPAddress server(44, 195, 202, 69);
+const char* device_unique_id = "MaskDetection_" __DATE__ "_" __TIME__;
+
+#define SERIAL_OUTPUT
+#ifdef SERIAL_OUTPUT
+#define PRINT(x)   Serial.print(x)
+#define PRINTLN(x) Serial.println(x)
+#else
+#define PRINT(x)   
+#define PRINTLN(x) 
+#endif
+
+// Enter a MAC address and IP address for your controller below.
+// The IP address will be dependent on your local network:
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 38, 0x77, 0xF7
+};
+IPAddress ip(192, 168, 0, 77);
+//IPAddress ip(10, 5, 15, 109);
+
+// Enter the IP address of the server you're connecting to:
+//IPAddress server(192, 168, 100, 2);
+//IPAddress server(10, 5, 15, 78);
+IPAddress server(10, 21, 70, 16);
+IPAddress myDns(192, 168, 0, 1);
+
+IPAddress mqtt_server(10, 21, 70, 16);
+// IPAddress mqtt_server(44, 195, 202, 69);
 
 // MQTT broker details
 const char* mqtt_topic = "mask_alert";
 
-// Buzzer setup
-const int buzzerPin = 5; // GP1 in WIZnet modules is typically mapped to Arduino digital pin 1
+const int buzzerPin = 15;
 
-EthernetClient ethClient;
-PubSubClient client(ethClient);
+EthernetClient mqttClient;
+PubSubClient mqtt_client(mqttClient);
+
+//===================================================================================================
 
 
 void callback(char* topic, byte* payload, unsigned int length) {
@@ -23,52 +49,90 @@ void callback(char* topic, byte* payload, unsigned int length) {
     message += (char)payload[i];
   }
 
-  Serial.print("Received message: ");
   Serial.println(message);
 
-  if (message == "Mask not detected!") {
-    // Buzz the buzzer for 2 seconds (adjust as needed)
+  if (message.equals("Mask not detected!")) {
+    Serial.println("Activating buzzer.");
     digitalWrite(buzzerPin, HIGH);
-    delay(2000);
+    delay(1000);
     digitalWrite(buzzerPin, LOW);
+    Serial.println("Buzzer deactivated.");
   }
 }
 
 void reconnect() {
-  while (!client.connected()) {
-    Serial.println("Attempting MQTT connection...");
-    if (client.connect("arduinoClient38")) {
-      Serial.println("Connected to MQTT broker");
-        // Once connected, publish an announcement...
-      client.publish("Out", "hello world");
+  // Loop until we're reconnected
+  while (!mqtt_client.connected()) {
+    PRINT("MQTT: Attempting MQTT connection...");
+    // Attempt to connect
+    if (mqtt_client.connect(device_unique_id)) {
+      PRINTLN("MQTT: connected");
       // ... and resubscribe
-      client.subscribe(mqtt_topic);
+      mqtt_client.subscribe(mqtt_topic);
     } else {
-      Serial.print("Connection failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" Retrying in 5 seconds...");
-      delay(5000);
+      PRINT("MQTT: failed, rc=");
+      PRINT(mqtt_client.state());
+      PRINTLN(" MQTT: try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(2000);
     }
   }
 }
 
-void setup() {
-  
-  Serial.begin(57600);
-  Ethernet.init(17);
-  client.setServer(server, 1883);
-  client.setCallback(callback);
+//===================================================================================================
 
-  Ethernet.begin(mac);
-  delay(1500);
+void setup() {
+    // Open serial communications and wait for port to open:
+  Serial3.setRx(PC11);
+  Serial3.setTx(PC10);  
+  delay(50);
+  
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
 
   pinMode(buzzerPin, OUTPUT);
+
+   // start the Ethernet connection:
+  PRINTLN("Initialize Ethernet with DHCP:");
+  if (Ethernet.begin(mac) == 0) {
+    PRINTLN("Failed to configure Ethernet using DHCP");
+    // Check for Ethernet hardware present
+    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+      PRINTLN("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+      while (true) {
+        delay(1); // do nothing, no point running without Ethernet hardware
+      }
+    }
+    if (Ethernet.linkStatus() == LinkOFF) {
+      PRINTLN("Ethernet cable is not connected.");
+    }
+    // try to congifure using IP address instead of DHCP:
+    Ethernet.begin(mac, ip, myDns);
+  } else {
+    PRINT("  DHCP assigned IP ");
+    PRINTLN(Ethernet.localIP());
+  }
   
+  // give the Ethernet shield a second to initialize:
+  delay(1000);
+
+  mqtt_client.setServer(mqtt_server, 1883);
+  mqtt_client.setCallback(callback);
+
+  // give the Ethernet shield a second to initialize:
+  delay(1000);
+
 }
 
 void loop() {
-  if (!client.connected()) {
+
+  if (!mqtt_client.connected()) {
     reconnect();
   }
-  client.loop();
+  mqtt_client.loop();
+
 }
